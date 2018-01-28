24d8b995c80e23f23c325167e1ae4dd7b7832d70
ADAM-PC\adamz <adamzimnyy@gmail.com>
2018-01-28 06:34:25 +0100

    inner search route fix

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -31,7 +31,8 @@ export class DetailsComponent implements OnInit {
 
   onSearch(name: string) {
     this.isRefreshing = true;
-    this.router.navigate(['/character', name.charAt(0).toUpperCase() + name.toLowerCase().slice(1)])
+    this.getCharacter(name)
+  //  this.router.navigate(['/character', name.charAt(0).toUpperCase() + name.toLowerCase().slice(1)])
   }
 
   ngOnInit() {
