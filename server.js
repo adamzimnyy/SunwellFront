0bb1c89c3865174293c3950026c0856583831aae
ADAM-PC\adamz <adamzimnyy@gmail.com>
2018-01-29 01:23:23 +0100

    configure origins2

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 //Install express server
 const express = require('express');
 const app = express();
-app.use(allowCrossDomain);
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 
