1996bdc03cb15e32c329c4cb3daf55f866d98af2
ADAM-PC\adamz <adamzimnyy@gmail.com>
2018-01-29 01:18:02 +0100

    change title to "... gearscore"

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 //Install express server
 const express = require('express');
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.configure(function () {
+  app.use(allowCrossDomain);
+  app.use(express.static(__dirname + '/dist'));
+  app.listen(process.env.PORT || 8080);
+});
