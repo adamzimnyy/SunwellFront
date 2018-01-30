e6e9c67b72ad0b6b078db8f4c3f0ba51a9c3fca8
ADAM-PC\adamz <adamzimnyy@gmail.com>
2018-01-30 01:44:42 +0100

    add online part1

---
+++ src/app/search/online.ts
@@ -0,0 +1,8 @@
+
+export interface Online {
+
+  id: number;
+  ferpnis: number;
+  angrathar: number;
+  date: Date;
+}
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,17 +1,38 @@
 import {Component, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
+import {Observable} from "rxjs/Observable";
+import {Online} from "./online";
 
+import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
 
-  constructor(private router: Router) {
+  readonly ROOT_URL = 'https://sunwell-back.herokuapp.com/online';
+
+  onlineObs: Observable<Online[]>;
+  online: Online[];
+
+  constructor(private http: HttpClient, private router: Router) {
   }
 
   onSearch(name: string) {
     this.router.navigate(['/character', name])
+  }
+
+  ngOnInit() {
+    this.getOnline()
+  }
+
+  getOnlineObservable(): Observable<Online[]> {
+    this.onlineObs = this.http.get<Online[]>(this.ROOT_URL);
+    return this.onlineObs;
+  }
+
+  getOnline() {
+    this.getOnlineObservable().subscribe(value => this.online = value);
   }
 }
