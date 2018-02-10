b9a282342fecc37806f6ea6208f9c47057141323
ADAM-PC\adamz <adamzimnyy@gmail.com>
2018-02-10 11:38:15 +0100

    scatter graph

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -104,7 +104,7 @@ export class SearchComponent implements OnInit {
         },
         elements: {
           point: {
-            radius: 0,
+            radius: 1,
             hoverRadius: 5,
           }
         },
